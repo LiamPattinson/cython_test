184f5ff055b5cff58fee3595645600593f829bec
Liam Pattinson <liampattinson@gmail.com>
2023-05-16 11:58:02 +0100

    Fix setuptools package discovery

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 from Cython.Build import cythonize
 
-setup(ext_modules = cythonize("xpdfpghljk/hello.pyx"))
+setup(ext_modules = cythonize("xpdfpghljk/hello.pyx", language_level=3))
